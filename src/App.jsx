f0708ee795677354006e10342efc3a678447e48e
Louie Schoenknecht <luisesch97@gmail.com>
2021-06-08 12:00:12 -0700

    configure dynamodb for song list app

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import './App.css';
+import awsconfig from './aws-exports';
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import { listSongs } from './graphql/queries';
+
+Amplify.configure(awsconfig);
+
+function App() {
+  const [songs, setSongs] = useState([]);
+  
+  const fetchSongs = async () => {
+      try {
+          const songData = await API.graphql(graphqlOperation(listSongs));
+          const songList = songData.data.listSongs.items;
+          console.log('song list', songList);
+          setSongs(songList);
+      } catch (error) {
+          console.log('error on fetching songs', error);
+      }
+  };
+  
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+  
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <AmplifySignOut />
+        <h2>My App Content</h2>
+      </header>
+    </div>
+  )
+}
+
+
+export default withAuthenticator(App);
